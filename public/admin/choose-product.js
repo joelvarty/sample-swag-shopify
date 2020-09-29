2f459fd2c93dd45491aae72b79e651283b26f26a
Joel Varty <joelvarty@outlook.com>
2020-09-29 11:52:23 -0400

    v2

--- public/admin/choose-product.js
+++ public/admin/choose-product.js
@@ -1,125 +1,144 @@
 //
 // API Item Picker
 //
-var ChooseProductCustomField = function(){
-    /// <summary>The type definition of this Agility Custom Field Type.</summary>
-    var self = this;
+var ChooseProductCustomField = function () {
+	/// <summary>The type definition of this Agility Custom Field Type.</summary>
+	var self = this;
 
-    /// <field name="Label" type="String">The display name of the Custom Field Type</field>
-    self.Label = "Choose Shopify Product";
+	/// <field name="Label" type="String">The display name of the Custom Field Type</field>
+	self.Label = "Choose Shopify Product";
 
-    /// <field name="ReferenceName" type="String">The internal reference name of the Custom Field Type. Must not contain any special characters.</field>
-    self.ReferenceName = "ChooseShopifyProduct";
+	/// <field name="ReferenceName" type="String">The internal reference name of the Custom Field Type. Must not contain any special characters.</field>
+	self.ReferenceName = "ChooseShopifyProduct";
 
-    /// <field name="Template" type="String">The partial HTML template that represents your custom field. It can be an absolute path to a URL or a reference name to an inline code file in Agility CMS. Your ViewModel will be automatically bound to this template.</field>
-    self.Template = "<input class='offer-picker-field' type='hidden' style='width:100%' data-bind='value:selectedValue,select2:select2'>";
+	/// <field name="Render" type="Function">This function runs every time the field is rendered</field>
+	self.Render = function (options) {
+		/// <summary>
+		///  The Render handler for this field.  Create any elements and bindings that you might need, pull down resources.
+		///  This method will be called everytime to the field value changes.
+		/// </summary>
+		/// <param name="options" type="ContentManager.Global.CustomInputFieldParams">The options used to render this field.</param>
 
-    /// <field name="Render" type="Function">This function runs every time the field is rendered</field>
-    self.Render = function(options){
-      //nothing needed here...
-    }
 
-    /// <field name="ViewModel" type="KO ViewModel">The KO ViewModel that will be binded to your HTML template</field>
-    self.ViewModel = function (options) {
 
-        /// <summary>The KO ViewModel that will be binded to your HTML template.</summary>
-        /// <param name="options" type="Object">
-        ///     <field name="$elem" type="jQueryElem">The .field-row jQuery Dom Element.</field>
-        ///     <field name="contentItem" type="ContentItem Object">The entire Content Item object including Values and their KO Observable properties of all other fields on the form.</field>
-        ///     <field name="fieldBinding" type="KO Observable">The value binding of thie Custom Field Type. Get and set this field's value by using this property.</field>
-        ///     <field name="fieldSetting" type="Object">Object representing the field's settings such as 'Hidden', 'Label', and 'Description'</field>
-        ///     <field name="readonly" type="boolean">Represents if this field should be readonly or not.</field>
-        /// </param>
-        var self = this;
+		//get our base element
+		var $pnl = $(".product-picker-field", options.$elem);
 
-    self.ajaxRequest = null;
+		if ($pnl.size() == 0) {
 
-        self.selectedValue = options.fieldBinding.extend({ throttle: 500 });
+			var htmlContent = "<input class='product-picker-field' type='hidden' style='width:100%' data-bind='value:selectedValue,select2:select2'>";
+			//pull down the html template and load it into the element
+			options.$elem.append(htmlContent)
 
-        self.formatResult = function (item) {
-            return item.title;
-        };
+			$pnl = $(".product-picker-field", options.$elem);
 
-        self.formatSelection = function (item) {
-            return item.title;
-        };
-        self.ajaxRequest = null;
 
-        self.select2 = {
-            label: 'Product',
-            readOnly: false,
-            value: options.fieldBinding,
-            multiple: false,
-            maximumSelectionSize: 1,
-            minimumInputLength: 0,
-            placeholder: 'Find product...',
-            formatResult: self.formatResult,
-            formatSelection: self.formatSelection,
+			//bind our viewmodel to this
+			var viewModel = function (options) {
 
-            matcher: function(term, text) {
-                return true;
-            },
+				/// <summary>The KO ViewModel that will be binded to your HTML template.</summary>
+				/// <param name="options" type="Object">
+				///     <field name="$elem" type="jQueryElem">The .field-row jQuery Dom Element.</field>
+				///     <field name="contentItem" type="ContentItem Object">The entire Content Item object including Values and their KO Observable properties of all other fields on the form.</field>
+				///     <field name="fieldBinding" type="KO Observable">The value binding of thie Custom Field Type. Get and set this field's value by using this property.</field>
+				///     <field name="fieldSetting" type="Object">Object representing the field's settings such as 'Hidden', 'Label', and 'Description'</field>
+				///     <field name="readonly" type="boolean">Represents if this field should be readonly or not.</field>
+				/// </param>
+				var self = this;
 
-            id: function (obj) {
-                //set content of the Agility CMS Content Item
+				self.ajaxRequest = null;
 
-                //options.contentItem.Values.ExternalID(obj.ID)
-                //options.contentItem.Values.Title(obj.Title);
-                //options.contentItem.Values.MyField1(obj.Value1)
-                //options.contentItem.Values.MyField2(obj.Value2)
-                //etc...
+				self.selectedValue = options.fieldBinding.extend({ throttle: 500 });
 
-                //return the ID
-                return obj.ID;
-            },
+				self.formatResult = function (item) {
+					return item.title;
+				};
 
-            ajax: { // instead of writing the function to execute the request we use Select2's convenient helper
-                url: "https://agility-cms-swag-test.myshopify.com/admin/api/graphql.json",
-                dataType: 'json',
-                type: "post",
-				quietMillis: 250,
-				headers: {
+				self.formatSelection = function (item) {
+					return item.title;
+				};
+				self.ajaxRequest = null;
 
-				},
-                originalValue: ko.unwrap(options.contentItem.Values.ExternalID()),
-                term: "",
-                data: function (term, page, params) {
-					console.log("results", term, page, params)
-                    return {
-                        q: term, // search term
-                        //other params...
-                        languageCode: ContentManager.ViewModels.Navigation.currentLanguageCode()
-                    };
-                },
-                results: function (data, page) {
-					console.log("results", data, page)
-                    //ensure data has both a 'Label' and 'Value' property in each item in the array
-                    return {
-                        results: data
-                    };
-                },
-                current: function(data){
-                    console.log('current', data);
-                },
-                cache: true
-            },
-            initSelection: function (element, callback) {
-                var val = ko.unwrap(options.fieldBinding);
-                var label = ko.unwrap(options.contentItem.Values.Title);
+				self.select2 = {
+					label: 'Product',
+					readOnly: false,
+					value: options.fieldBinding,
+					multiple: false,
+					maximumSelectionSize: 1,
+					minimumInputLength: 0,
+					placeholder: 'Find product...',
+					formatResult: self.formatResult,
+					formatSelection: self.formatSelection,
 
-                if(val && label) {
-                    var data = {
-                        Value: val,
-                        Label: label
-                    };
+					matcher: function (term, text) {
+						return true;
+					},
 
-                    callback(data);
-                }
-            },
-            allowClear: false,
-            dropdownCssClass: "bigdrop"
-        };
-    }
+					id: function (obj) {
+						//set content of the Agility CMS Content Item
+
+						//options.contentItem.Values.ExternalID(obj.ID)
+						//options.contentItem.Values.Title(obj.Title);
+						//options.contentItem.Values.MyField1(obj.Value1)
+						//options.contentItem.Values.MyField2(obj.Value2)
+						//etc...
+
+						//return the ID
+						return obj.ID;
+					},
+
+					ajax: { // instead of writing the function to execute the request we use Select2's convenient helper
+						url: "https://agility-cms-swag-test.myshopify.com/admin/api/graphql.json",
+						dataType: 'json',
+						type: "post",
+						quietMillis: 250,
+						headers: {
+
+						},
+						originalValue: ko.unwrap(options.contentItem.Values.ExternalID()),
+						term: "",
+						data: function (term, page, params) {
+							console.log("results", term, page, params)
+							return {
+								q: term, // search term
+								//other params...
+								languageCode: ContentManager.ViewModels.Navigation.currentLanguageCode()
+							};
+						},
+						results: function (data, page) {
+							console.log("results", data, page)
+							//ensure data has both a 'Label' and 'Value' property in each item in the array
+							return {
+								results: data
+							};
+						},
+						current: function (data) {
+							console.log('current', data);
+						},
+						cache: true
+					},
+					initSelection: function (element, callback) {
+						var val = ko.unwrap(options.fieldBinding);
+						var label = ko.unwrap(options.contentItem.Values.Title);
+
+						if (val && label) {
+							var data = {
+								Value: val,
+								Label: label
+							};
+
+							callback(data);
+						}
+					},
+					allowClear: false,
+					dropdownCssClass: "bigdrop"
+				};
+			}
+
+			ko.applyBindings(viewModel, $pnl.get(0));
+		}
+
+	}
 }
 
 ContentManager.Global.CustomInputFormFields.push(new ChooseProductCustomField());
